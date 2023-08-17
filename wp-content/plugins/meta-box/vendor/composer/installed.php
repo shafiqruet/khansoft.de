44fc156a89c7adce4eed498c713c78c28d2a9328
Touhid <touhidulislamtwahid@gmail.com>
2023-08-17 13:21:28 +0600

    initial commit

---
+++ wp-content/plugins/meta-box/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmetabox/meta-box',
+        'pretty_version' => '5.7.4',
+        'version' => '5.7.4.0',
+        'reference' => 'cd17d25946b2c48568cef5ca996dde4eb2736a38',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wpmetabox/meta-box' => array(
+            'pretty_version' => '5.7.4',
+            'version' => '5.7.4.0',
+            'reference' => 'cd17d25946b2c48568cef5ca996dde4eb2736a38',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
